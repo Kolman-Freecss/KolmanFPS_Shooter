b89750af7d072cd65cecf5cbff8adad348a10510
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2023-11-13 23:23:08 +0100

    OK Task (Skins) -> Relevant tasks DONE:
       - SO Removed
       - Monobehaviour attached to the SkinView(TPS, FPS)

---
+++ Assets/Scripts/Entities/Player/Skin/PlayerSkin.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Entities.Camera;
+using Entities.Utils;
+using UnityEngine;
+
+namespace Entities.Player
+{
+    public class PlayerSkin : MonoBehaviour
+    {
+        [Description("Skin")]
+        [SerializeField]
+        Player.PlayerSkin _skin;
+        
+        public Player.PlayerSkin SkinValue
+        {
+            get => _skin;
+        }
+        
+        [Description("Skin View - Positions of the player's body parts")]
+        [SerializeField]
+        List<SerializableDictionaryEntry<CameraMode, SkinView>> _skinViews;
+        
+        public List<SerializableDictionaryEntry<CameraMode, SkinView>> SkinViewsValue
+        {
+            get => _skinViews;
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/Player/Skin/SkinParts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Entities.Player
+{
+    /// <summary>
+    /// Positions of the player's body parts
+    /// </summary>
+    [System.Serializable]
+    public class SkinParts
+    {
+        [SerializeField]
+        private Transform m_rightHand;
+        public Transform RightHand => m_rightHand;
+        
+        [SerializeField]
+        private Transform m_leftHand;
+        public Transform LeftHand => m_leftHand;
+        
+        [SerializeField]
+        private Transform m_head;
+        public Transform Head => m_head;
+    }
+}
